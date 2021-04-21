cc4b2322f251b3f2c7865925f92aef8e5224c262
Parth Patel <pparth2098@gmail.com>
2021-04-21 01:53:14 -0400

    Create Login Page using Bootstrap

---
+++ my-stote/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+import { faCartArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="text-center">
+        <h1>Header</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
