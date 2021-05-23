5458a36162aaec3bb3738876e349e14d84dd6b41
Parth Patel <pparth2098@gmail.com>
2021-05-23 15:30:19 -0400

    Install redux and create a redux store. - connect the store to chrom dev tool

---
+++ my-stote/src/data.js
@@ -0,0 +1,117 @@
+const data = {
+  products: [
+    {
+      _id: "1",
+      name: "Toor Dal",
+      category: "Grocery",
+      subCategory: "Dal & Pulses",
+      prodImage:
+        "https://5.imimg.com/data5/RO/UP/MY-5329745/tata-sampann-chana-dal-500x500.jpg",
+      price: 99,
+      sellPrice: 59,
+      description:
+        "The pigeon pea, also known as pigeonpea, red gram, tur, or gungo peas, gungu peas, gunga peas in Jamaica, is a perennial legume from the family Fabaceae. Since its domestication in the Indian subcontinent at least 3,500 years ago, its seeds have become a common food in Asia, Africa, and Latin America.",
+      stock: 1,
+    },
+
+    {
+      _id: "2",
+      name: "Moong Dal",
+      category: "Grocery",
+      subCategory: "Dal & Pulses",
+      prodImage:
+        "https://5.imimg.com/data5/RO/UP/MY-5329745/tata-sampann-chana-dal-500x500.jpg",
+      price: 99,
+      sellPrice: 59,
+      description:
+        "The pigeon pea, also known as pigeonpea, red gram, tur, or gungo peas, gungu peas, gunga peas in Jamaica, is a perennial legume from the family Fabaceae. Since its domestication in the Indian subcontinent at least 3,500 years ago, its seeds have become a common food in Asia, Africa, and Latin America.",
+      stock: 2,
+    },
+
+    {
+      _id: "3",
+      name: "Channa Dal",
+      category: "Grocery",
+      subCategory: "Dal & Pulses",
+      prodImage:
+        "https://5.imimg.com/data5/RO/UP/MY-5329745/tata-sampann-chana-dal-500x500.jpg",
+      price: 99,
+      sellPrice: 59,
+      description:
+        "The pigeon pea, also known as pigeonpea, red gram, tur, or gungo peas, gungu peas, gunga peas in Jamaica, is a perennial legume from the family Fabaceae. Since its domestication in the Indian subcontinent at least 3,500 years ago, its seeds have become a common food in Asia, Africa, and Latin America.",
+      stock: 1,
+    },
+
+    {
+      _id: "4",
+      name: "Arhar Dal",
+      category: "Grocery",
+      subCategory: "Dal & Pulses",
+      prodImage:
+        "https://5.imimg.com/data5/RO/UP/MY-5329745/tata-sampann-chana-dal-500x500.jpg",
+      price: 99,
+      sellPrice: 59,
+      description:
+        "The pigeon pea, also known as pigeonpea, red gram, tur, or gungo peas, gungu peas, gunga peas in Jamaica, is a perennial legume from the family Fabaceae. Since its domestication in the Indian subcontinent at least 3,500 years ago, its seeds have become a common food in Asia, Africa, and Latin America.",
+      stock: 0,
+    },
+
+    {
+      _id: "5",
+      name: "Toor Dal",
+      category: "Grocery",
+      subCategory: "Dal & Pulses",
+      prodImage:
+        "https://5.imimg.com/data5/RO/UP/MY-5329745/tata-sampann-chana-dal-500x500.jpg",
+      price: 99,
+      sellPrice: 59,
+      description:
+        "The pigeon pea, also known as pigeonpea, red gram, tur, or gungo peas, gungu peas, gunga peas in Jamaica, is a perennial legume from the family Fabaceae. Since its domestication in the Indian subcontinent at least 3,500 years ago, its seeds have become a common food in Asia, Africa, and Latin America.",
+      stock: 5,
+    },
+
+    {
+      _id: "6",
+      name: "Moong Dal",
+      category: "Grocery",
+      subCategory: "Dal & Pulses",
+      prodImage:
+        "https://5.imimg.com/data5/RO/UP/MY-5329745/tata-sampann-chana-dal-500x500.jpg",
+      price: 99,
+      sellPrice: 59,
+      description:
+        "The pigeon pea, also known as pigeonpea, red gram, tur, or gungo peas, gungu peas, gunga peas in Jamaica, is a perennial legume from the family Fabaceae. Since its domestication in the Indian subcontinent at least 3,500 years ago, its seeds have become a common food in Asia, Africa, and Latin America.",
+      stock: 16,
+    },
+
+    {
+      _id: "7",
+      name: "Channa Dal",
+      category: "Grocery",
+      subCategory: "Dal & Pulses",
+      prodImage:
+        "https://5.imimg.com/data5/RO/UP/MY-5329745/tata-sampann-chana-dal-500x500.jpg",
+      price: 999,
+      sellPrice: 59,
+      description:
+        "The pigeon pea, also known as pigeonpea, red gram, tur, or gungo peas, gungu peas, gunga peas in Jamaica, is a perennial legume from the family Fabaceae. Since its domestication in the Indian subcontinent at least 3,500 years ago, its seeds have become a common food in Asia, Africa, and Latin America.",
+      stock: 1,
+    },
+
+    {
+      _id: "8",
+      name: "Arhar Dal",
+      category: "Grocery",
+      subCategory: "Dal & Pulses",
+      prodImage:
+        "https://5.imimg.com/data5/RO/UP/MY-5329745/tata-sampann-chana-dal-500x500.jpg",
+      price: 9,
+      sellPrice: 59,
+      description:
+        "The pigeon pea, also known as pigeonpea, red gram, tur, or gungo peas, gungu peas, gunga peas in Jamaica, is a perennial legume from the family Fabaceae. Since its domestication in the Indian subcontinent at least 3,500 years ago, its seeds have become a common food in Asia, Africa, and Latin America.",
+      stock: 10,
+    },
+  ],
+};
+
+export default data;
--- my-stote/src/index.js
+++ my-stote/src/index.js
@@ -1,12 +1,14 @@
-  import React from 'react';
-  import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import store from "./store";
 ReactDOM.render(
-  
-    <App />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
 );
-
-
